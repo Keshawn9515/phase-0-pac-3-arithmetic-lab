018d83c085b15af7d96612cf55a3721dfa6e87a5
Keshawn <Keshawn9515@icloud.com>
2023-03-21 09:41:03 -0700

    defining basic math functions

---
+++ index.js
@@ -0,0 +1,75 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  module.exports = {
+    add,
+  };
+
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  module.exports = {
+    subtract,
+  };
+
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  module.exports = {
+    multiply,
+  };
+
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  module.exports = {
+    divide,
+  };
+
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return ++n;
+  }
+  
+  function decrement(n) {
+    return --n;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    increment,
+    decrement,
+    makeInt,
+    preserveDecimal,
+  };
+  
